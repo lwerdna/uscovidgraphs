0994e43e701a05db3edc894dd2b24f6a264eb7fb
Andrew Lamoureux <andrew@vector35.com>
2020-04-08 22:56:23 -0400

    Capture negative and positive for each sample

--- go.py
+++ go.py
@@ -88,16 +88,20 @@ def csv_load():
 	with open('./daily.csv') as fp:
 		lines = [x.strip() for x in fp.readlines()]
 
-	assert lines[0] == 'date,state,positive,negative,pending,hospitalized,death,total,hash,dateChecked,totalTestResults,fips,deathIncrease,hospitalizedIncrease,negativeIncrease,positiveIncrease,totalTestResultsIncrease'
+	if not lines[0].startswith('date,state,positive,negative'):
+		raise Exception('unexpected csv header: %s' % lines[0])
+
 	lines = lines[1:]
 
 	for line in lines:
-		(date,state,positive,_,_,_,_,_,_,_,_,_,_,_,_,_,_) = line.split(',')
+		fields = line.split(',')
+		(date,state,positive,negative) = fields[0:4]
 		if not state in data:
 			data[state] = {}
 		#print('appending %s (%s,%s)' % (state, date, positive))
 		if positive == '': positive = 0
-		data[state][date] = int(positive)
+		if negative == '': negative = 0
+		data[state][date] = {'positive':int(positive), 'negative':int(negative)}
 
 	# check for missing dates
 	now = time.time()
@@ -118,20 +122,26 @@ def csv_load():
 			cur += 24*3600
 
 	# generate 'US'
-	earliest = list(data['AK'].keys())[0]
+	earliest = latest = list(data['AK'].keys())[0]
 	for state in state_abbrevs:
 		earliest = min(earliest, min(data[state].keys()))
+		latest = max(latest, max(data[state].keys()))
+	print('latest date is: %s' % latest)
 	m = re.match(r'^\d\d\d\d(\d\d)(\d\d)', earliest)
 	cur = ISO8601ToEpoch('2020-%s-%s' % (m.group(1), m.group(2)))
 
 	data['US'] = {}
 	while cur < now:
 		curstr = epochToISO8601(cur).replace('-','')
-		total = 0
+		positive_all_states = 0
+		negative_all_states = 0
 		for state in state_abbrevs:
-			total += data[state].get(curstr, 0)
-		if total == 0: break
-		data['US'][curstr] = total
+			if not curstr in data[state]:
+				continue
+			positive_all_states += data[state][curstr]['positive']
+			negative_all_states += data[state][curstr]['negative']
+		if positive_all_states == 0: break
+		data['US'][curstr] = {'positive':positive_all_states, 'negative':negative_all_states}
 		cur += 24*3600
 
 	state_abbrevs.append('US')
@@ -145,7 +155,7 @@ def write_gnuplot(state):
 	for date in sorted(data[state]):
 		# mm/dd
 		xtics.append('%s/%s' % re.match(r'^\d\d\d\d(\d\d)(\d\d)$', date).group(1,2))
-		positives.append(data[state][date])
+		positives.append(data[state][date]['positive'])
 
 	(max_, min_) = (max(positives), min(positives))
 
@@ -156,12 +166,12 @@ def write_gnuplot(state):
 		fp.write('set term png\n')
 		fp.write('set output "./graphs/%s.png\n' % (state))
 		fp.write('set style data linespoints\n')
-		fp.write('set key box\n')
+		fp.write('set key box opaque\n')
 		fp.write('set style line 1 lt 2 lw 9 pt 9 ps 0.5\n')
-		fp.write('set xtics rotate by -65\n')
-		fp.write('set xtics font \',8\'\n')
+		fp.write('set xtics rotate by -90\n')
+		fp.write('set xtics font \',10\'\n')
 		#fp.write('set bmargin 2\n')
-		#fp.write('set lmargin 0\n')
+		fp.write('set lmargin 8\n')
 		#fp.write('set rmargin 0\n')
 		#fp.write('set tmargin 0\n')
 		if max_ - min_ > 100:
@@ -207,15 +217,15 @@ def write_gnuplot(state):
 		fp.write('"$data" using 0:2:(($1 == %d || $1 == %d) ? sprintf("%%d            ",$2) : "") with labels notitle textcolor rgb "#0000FF"\n' % (idx_t1, idx_t1))
 
 		# second plot, rate of change
-		fp.write('set key right bottom\n')
-		fp.write('set yrange [0 : 2]\n')
+		fp.write('set key right top\n')
+		fp.write('set yrange [1 : 1.75]\n')
 		fp.write('plot "$data" using 1:3:xtic(4) title "daily rate increase" linecolor rgb "#0000FF"\n')
 
 
 def html(states=state_abbrevs):
 	global data
 
-	state2total = {state:max(data[state].values()) for state in state_abbrevs}
+	state2total = {state:max([data[state][date]['positive'] for date in data[state].keys()]) for state in state_abbrevs}
 	worst2best = sorted(state2total, key=lambda x: state2total[x], reverse=True)
 	for state in worst2best:
 		print('state %s has %d cases' % (state, state2total[state]))
@@ -266,8 +276,8 @@ if __name__ == '__main__':
 
 	if command == 'update':
 		csv_update()
-	elif command == 'load':
 		csv_load()
+
 	elif command == 'graph':
 		csv_load()
 
